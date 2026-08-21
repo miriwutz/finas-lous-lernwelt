c1184bb37eb2738d996d18112e391c724097306c
miriwutz <miriwutz@gmail.com>
2026-08-21 14:03:58 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import {
   ensureSpace, onSnapshot, updateDoc, runTransaction, serverTimestamp
 } from "./firebase.js";
 
-const APP_VERSION = "2.5a 50 Blätter & Blümchen";
+const APP_VERSION = "2.5b Kindgerechter Baum-Infobereich";
 
 const $ = (s) => document.querySelector(s);
 const $$ = (s) => [...document.querySelectorAll(s)];
